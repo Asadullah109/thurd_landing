a81281959849c03af85c845ca16b6b6dd98ee44b
Asadullah109 <147506148+Asadullah109@users.noreply.github.com>
2024-12-22 00:01:38 +0500

    d

---
+++ page.tsx
@@ -0,0 +1,14 @@
+import Navbar from "@/components/Navbar";
+import Galary from "@/components/Galary";
+import Footer from "@/components/Footer";
+export default function Home(){
+
+  return(
+    <div>
+      <Navbar/>
+      <Galary/>
+      <Footer/>
+    </div>
+  )
+  
+}
